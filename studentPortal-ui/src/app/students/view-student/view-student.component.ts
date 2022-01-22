491f9d5cf8f179591a7c4c8f3df8a7116fcdaf31
Swiech-Jan <semper666@gmail.com>
2022-01-22 18:55:46 +0100

    Added Student Update to UI

--- studentPortal-ui/src/app/students/view-student/view-student.component.ts
+++ studentPortal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
@@ -37,7 +38,8 @@ export class ViewStudentComponent implements OnInit {
   constructor(
     private readonly studentService: StudentService,
     private readonly route: ActivatedRoute,
-    private readonly genderService: GenderService
+    private readonly genderService: GenderService,
+    private snackbar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -55,5 +57,20 @@ export class ViewStudentComponent implements OnInit {
         });
       }
     });
+  }
+
+  onUpdate(): void {
+    this.studentService.updateStudent(this.student.id, this.student).subscribe(
+      (successResponse) => {
+        // Show a notification
+        this.snackbar.open('Student updated successfully', undefined, {
+          duration: 2000,
+        });
+      },
+      (errorResponse) => {
+        // Log it
+        console.log(errorResponse);
+      }
+    );
   }
 }
